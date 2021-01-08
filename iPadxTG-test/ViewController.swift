2c4cccf3799621b46be94b09b48e21ce40488848
ponte1010 <t.miwa.528@nitech.jp>
2021-01-08 21:40:02 +0900

    [add]タップした座標を取得してprintで出力

--- iPadxTG-test/ViewController.swift
+++ iPadxTG-test/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var tapLocation: CGPoint = CGPoint()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // タッチイベントを取得する
+        let touch = touches.first
+        // タップした座標を取得し、print
+        tapLocation = touch!.location(in: self.view)
+        print(tapLocation)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
