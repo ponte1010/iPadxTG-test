03b7fc8af0b363a11c3e90ff9773209ffd37cd03
ponte1010 <t.miwa.528@nitech.jp>
2021-01-08 23:14:51 +0900

    [add]座標の音声出力

--- iPadxTG-test/ViewController.swift
+++ iPadxTG-test/ViewController.swift
@@ -6,22 +6,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     var tapLocation: CGPoint = CGPoint()
     
+    // AVSpeechSynthesizerのインスタンスを生成
+    let synthesizer = AVSpeechSynthesizer()
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // タッチイベントを取得する
         let touch = touches.first
-        // タップした座標を取得し、print
+        // タップした座標を取得
         tapLocation = touch!.location(in: self.view)
-        print(tapLocation)
+        
+        let xTap = tapLocation.x
+        let yTap = tapLocation.y
+        
+        let intxTap:Int = Int(xTap)
+        let intyTap:Int = Int(yTap)
+        
+        let strxTap:String = String(intxTap)
+        let stryTap:String = String(intyTap)
+        
+        let strTapPoints = "エックス" + strxTap + "ワイ" + stryTap
+        
+        // 確認用
+        print(strTapPoints)
+        
+        let utterance = AVSpeechUtterance.init(string: strTapPoints)
+        let voice = AVSpeechSynthesisVoice.init(language: "ja-JP")
+        utterance.voice = voice
+        synthesizer.speak(utterance)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     
