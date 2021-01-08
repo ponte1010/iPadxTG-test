fe81599d5939bb903d6036d173df494c3ea8e3ff
ponte1010 <t.miwa.528@nitech.jp>
2021-01-08 20:45:24 +0900

    Initial Commit

---
+++ iPadxTG-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPadxTG-test
+//
+//  Created by miwa on 2021/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
